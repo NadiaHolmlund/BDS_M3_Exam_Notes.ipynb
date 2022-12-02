5c79be8510d87111671161c902038e0c2fcbc7e5
Nadia <112628369+NadiaHolmlund@users.noreply.github.com>
2022-12-02 15:19:46 +0100

    xv

--- Home.py
+++ Home.py
@@ -1,8 +1,12 @@
 import streamlit as st
 
+# Before loading the data, we specify page configurations.
+# Page_title and page_icon determines the layout of the webbrowser-tab.
+# Layout determines the overall layout of the page. Wide specifies that the whole screen width is used for displaying the app.
 st.set_page_config(
-    page_title="miltipage app"
-)
+    page_title="⚽ Homepage",
+    page_icon="⚽",
+    layout="wide")
 
 st.title("Intelligent Scouting and Player Assessment")
 st.sidebar.success("select a page above")
