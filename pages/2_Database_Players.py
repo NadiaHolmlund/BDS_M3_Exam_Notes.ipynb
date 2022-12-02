55825985b1eb1254588dc4c2df95e767aae789de
Nadia <112628369+NadiaHolmlund@users.noreply.github.com>
2022-12-02 17:25:27 +0100

    sdf

--- pages/2_Database_Players.py
+++ pages/2_Database_Players.py
@@ -2,9 +2,8 @@ import streamlit as st
 
 col1, col2 = st.columns(2)
 
-
-
-col1, col2, col3 = st.columns(3)
-col1.metric("Temperature", "70 °F", "1.2 °F")
-col2.metric("Wind", "9 mph", "-8%")
-col3.metric("Humidity", "86%", "4%")
+with col1:
+    col1, col2, col3 = st.columns(3)
+    col1.metric("Temperature", "70 °F", "1.2 °F")
+    col2.metric("Wind", "9 mph", "-8%")
+    col3.metric("Humidity", "86%", "4%")
