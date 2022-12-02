e01726f6444fe3a4bc2d4c52848e4b531f3b07f1
Nadia <112628369+NadiaHolmlund@users.noreply.github.com>
2022-12-02 17:14:44 +0100

    sdf

--- pages/2_Database_Players.py
+++ pages/2_Database_Players.py
@@ -6,7 +6,7 @@ col1, col2 = st.columns(2)
 with col1:
     st.metric(label="Temperature", value="70 °F", delta="1.2 °F")
    
-if st.button(Compare players):
+if st.button('Compare players'):
 
     with col2:
         st.metric(label="Temperature", value="70 °F", delta="1.2 °F")
