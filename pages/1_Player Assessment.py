afcfe1b6f1f816f7399cf4abedc72cff5f4066bc
Nadia <112628369+NadiaHolmlund@users.noreply.github.com>
2022-12-02 16:38:50 +0100

    xv

--- pages/1_Player Assessment.py
+++ pages/1_Player Assessment.py
@@ -67,5 +67,5 @@ st.plotly_chart(fig, use_container_width=True)
 import streamlit as st
 from PIL import Image
 
-image = Image.open('NadiaHolmlund/BDS_M3_Exam_Notes.ipynb/app_images/GK.png')
+image = Image.open('app_images/GK.png')
 st.image(image)
