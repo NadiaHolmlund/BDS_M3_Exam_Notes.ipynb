ab5ded8364cdf94016b0640aa3e6b1592eafc531
Nadia <112628369+NadiaHolmlund@users.noreply.github.com>
2022-12-02 17:15:16 +0100

    df

--- pages/2_Database_Players.py
+++ pages/2_Database_Players.py
@@ -4,9 +4,5 @@ import streamlit as streamlit
 col1, col2 = st.columns(2)
 
 with col1:
-    st.metric(label="Temperature", value="70 °F", delta="1.2 °F")
-   
-if st.button('Compare players'):
 
-    with col2:
-        st.metric(label="Temperature", value="70 °F", delta="1.2 °F")
+with col2:
