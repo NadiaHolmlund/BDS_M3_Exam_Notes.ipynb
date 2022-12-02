3485479e2a45458773c72075cc6b8690c19b49f2
Nadia <112628369+NadiaHolmlund@users.noreply.github.com>
2022-12-02 17:14:09 +0100

    sdf

--- pages/2_Database_Players.py
+++ pages/2_Database_Players.py
@@ -1,3 +1,12 @@
 import streamlit as streamlit
 
-st.metric(label="Temperature", value="70 °F", delta="1.2 °F")
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.metric(label="Temperature", value="70 °F", delta="1.2 °F")
+   
+if st.button(Compare players):
+
+    with col2:
+        st.metric(label="Temperature", value="70 °F", delta="1.2 °F")
