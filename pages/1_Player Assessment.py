69dd395f7615bed0522945f1104f25132ffd53ba
Nadia <112628369+NadiaHolmlund@users.noreply.github.com>
2022-12-02 16:36:39 +0100

    sdf

--- pages/1_Player Assessment.py
+++ pages/1_Player Assessment.py
@@ -64,5 +64,7 @@ fig.update_layout(
 st.plotly_chart(fig, use_container_width=True)
 
 
+from PIL import Image
 
-st.image.open(NadiaHolmlund/BDS_M3_Exam_Notes.ipynb/app_images/DF.png)
+image = image.open('NadiaHolmlund/BDS_M3_Exam_Notes.ipynb/app_images/DF.png)')
+st.image(image)
