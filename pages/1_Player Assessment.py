1e9a11e55a26367121de260b9fe00ddc0e585685
Nadia <112628369+NadiaHolmlund@users.noreply.github.com>
2022-12-02 14:23:51 +0100

    h

--- pages/1_Player Assessment.py
+++ pages/1_Player Assessment.py
@@ -4,6 +4,6 @@ import streamlit as st
 # Page_title and page_icon determines the layout of the webbrowser-tab.
 # Layout determines the overall layout of the page. Wide specifies that the whole screen width is used for displaying the app.
 st.set_page_config(
-    page_title="Player Assessment 🔍",
-    page_icon="🔍",
+    page_title="Player Assessment ⚽",
+    page_icon="⚽",
     layout="wide")
