a6f5cfde3c0f3f1c9e448d4591b09f5f75956695
Nadia <112628369+NadiaHolmlund@users.noreply.github.com>
2022-12-02 16:04:38 +0100

    df

--- pages/1_Player Assessment.py
+++ pages/1_Player Assessment.py
@@ -31,3 +31,34 @@ st.write(name)
 
 
 import plotly.graph_objects as go
+
+
+categories = ['processing cost','mechanical properties','chemical stability',
+              'thermal stability', 'device integration']
+
+fig = go.Figure()
+
+fig.add_trace(go.Scatterpolar(
+      r=[1, 5, 2, 2, 3],
+      theta=categories,
+      fill='toself',
+      name='Product A'
+))
+fig.add_trace(go.Scatterpolar(
+      r=[4, 3, 2.5, 1, 2],
+      theta=categories,
+      fill='toself',
+      name='Product B'
+))
+
+fig.update_layout(
+  polar=dict(
+    radialaxis=dict(
+      visible=True,
+      range=[0, 5]
+    )),
+  showlegend=False
+)
+
+# Plot!
+st.plotly_chart(fig, use_container_width=True)
