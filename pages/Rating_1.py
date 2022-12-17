497a20564fee6dbfb1aab847767389ed6a3cf810
Nadia <112628369+NadiaHolmlund@users.noreply.github.com>
2022-12-17 18:20:22 +0100

    cb

--- pages/Rating_1.py
+++ pages/Rating_1.py
@@ -2,7 +2,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 import shap
 from streamlit_shap import st_shap
 import pickle
