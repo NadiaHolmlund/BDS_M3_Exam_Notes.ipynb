478ae3d58eb53676cbba335c4acb5cb4cf76b978
Nadia <112628369+NadiaHolmlund@users.noreply.github.com>
2022-12-17 18:32:58 +0100

    xdf

--- pages/Rating_1.py
+++ pages/Rating_1.py
@@ -28,7 +28,7 @@ st.markdown(
 
 
 # Setting up the default sidebar
-st.sidebar.expander('Player Information'):
+st.sidebar.expander('Player Information')
     player = st.sidebar.text_input((''),('Player'))
     col1, col2 = st.sidebar.columns(2)
     team = col1.text_input((''),('Team'))
