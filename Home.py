2a0c31ae1ceae1cc59d2633067e7cb8cf5ac476a
Nadia <112628369+NadiaHolmlund@users.noreply.github.com>
2022-12-17 17:38:34 +0100

    df

--- Home.py
+++ Home.py
@@ -26,55 +26,6 @@ with col1:
 
 with col2:
     st.write('')
-    st.subheader("Player Rating")
+    st.subheader("Feature")
     st.write("Feature presentation")
     st.write("Feature presentation")
-    demo_1 = st.button('Demo', key='demo_1')
-if demo_1:
-        video_file = open('/work/M3/Images/Screen Recording 2022-12-04 at 18.26.48.mov', 'rb')
-        video_bytes = video_file.read()
-        st.video(video_bytes)
-        close_demo_1 = st.button('Close Demo', key='close_demo_1')
-
-
-
-
-col1, col2 = st.columns(2)
-
-with col1:
-    picture_1 = Image.open('/work/M3/Images/blog-Pitchero_2-768x384-modified.png')
-    st.image(picture_1)
-
-with col2:
-    st.write('')
-    st.subheader("Player Comparison")
-    st.write("Feature presentation")
-    st.write("Feature presentation")
-    demo_2 = st.button('Demo', key='demo_2')
-if demo_2:
-        video_file = open('/work/M3/Images/Screen Recording 2022-12-04 at 18.26.48.mov', 'rb')
-        video_bytes = video_file.read()
-        st.video(video_bytes)
-        close_demo_2 = st.button('Close Demo', key='close_demo_2')
-
-
-
-
-
-col1, col2 = st.columns(2)
-
-with col1:
-    picture_1 = Image.open('/work/M3/Images/blog-Pitchero_2-768x384-modified.png')
-    st.image(picture_1)
-
-with col2:
-    st.write('')
-    st.subheader("Player Database")
-    st.write("Feature presentation")
-    st.write("Feature presentation")
-    demo_3 = st.button('Demo', key='demo_3')
-if demo_3:
-        video_file = open('/work/M3/Images/Screen Recording 2022-12-04 at 18.26.48.mov', 'rb')
-        video_bytes = video_file.read()
-        st.video(video_bytes)
-        close_demo_3 = st.button('Close Demo', key='close_demo_3')
