37582da38bfe5d66a8cae2ddbab605be5fa8a453
Nadia <112628369+NadiaHolmlund@users.noreply.github.com>
2022-12-17 18:32:06 +0100

    xcv

--- pages/Rating_1.py
+++ pages/Rating_1.py
@@ -28,6 +28,9 @@ st.markdown(
 
 
 # Setting up the default sidebar
+with st.sidebar
+    with st.expander('Hello)
+
 with st.sidebar.expander('Player Information'):
     player = st.sidebar.text_input((''),('Player'))
     col1, col2 = st.sidebar.columns(2)
