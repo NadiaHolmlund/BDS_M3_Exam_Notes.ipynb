80da51f0b54c10463d6713e4e10ec1156b223326
Nadia <112628369+NadiaHolmlund@users.noreply.github.com>
2022-12-18 18:03:33 +0100

    sdf

--- pages/Database_1.py
+++ pages/Database_1.py
@@ -16,4 +16,4 @@ st.set_page_config(
 with st.sidebar:
     st.radio('League', ('Bundesliga', 'Superliga'))
     st.radio('Year', ('2018', '2019', '2020', '2021', '2022'))
-    st.selectbox('Player', 'Player')
+    st.selectbox('Player', ('Select Player'))
